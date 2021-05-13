89c68d1405e6e480dfca55ddc66faa57d42dcdee
NickMonks <nicolasmg94@gmail.com>
2021-05-13 21:46:29 +0100

    Added BMI menu functionality; added Radio Group with selectors to change from metric to imperial

--- app/src/main/java/com/nickmonks/a7minuteworkout/BMIActivity.kt
+++ app/src/main/java/com/nickmonks/a7minuteworkout/BMIActivity.kt
@@ -2,8 +2,13 @@ package com.nickmonks.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_bmiactivity.*
 import kotlinx.android.synthetic.main.activity_finish.*
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.pow
 
 class BMIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +19,107 @@ class BMIActivity : AppCompatActivity() {
         val actionbar = supportActionBar
         if (actionbar != null){
             actionbar.setDisplayHomeAsUpEnabled(true) // set back button
-            actionbar.title = "CALCULATE YOUT BMI"
+            actionbar.title = "CALCULATE YOUR BMI"
         }
 
         // If we press the back button...
         toolbar_bmi_activity.setNavigationOnClickListener{
             onBackPressed()
         }
+
+        // button for BMI calculation
+        btnCalculateUnits.setOnClickListener {
+            if (validateMetricUnits()){
+                val heightValue : Float = etMetricUnitHeight.text.toString().toFloat() /100 //m
+                val weightValue : Float = etMetricUnitWeight.text.toString().toFloat() //kg
+
+                val bmi = weightValue / (heightValue.pow(2))
+                displayBMIResult(bmi)
+            }else{
+                Toast.makeText(this@BMIActivity,"Please enter valid values", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+    }
+
+    private fun displayBMIResult(bmi: Float){
+        val bmiLabel: String
+        val bmiDescription: String
+
+        if (java.lang.Float.compare(bmi, 15f) <= 0) {
+            bmiLabel = "Very severely underweight"
+            bmiDescription = "Oops! You really need to take care of your better! Eat more!"
+        } else if (java.lang.Float.compare(bmi, 15f) > 0 && java.lang.Float.compare(
+                bmi,
+                16f
+            ) <= 0
+        ) {
+            bmiLabel = "Severely underweight"
+            bmiDescription = "Oops! You really need to take care of your better! Eat more!"
+        } else if (java.lang.Float.compare(bmi, 16f) > 0 && java.lang.Float.compare(
+                bmi,
+                18.5f
+            ) <= 0
+        ) {
+            bmiLabel = "Underweight"
+            bmiDescription = "Oops! You really need to take care of your better! Eat more!"
+        } else if (java.lang.Float.compare(bmi, 18.5f) > 0 && java.lang.Float.compare(
+                bmi,
+                25f
+            ) <= 0
+        ) {
+            bmiLabel = "Normal"
+            bmiDescription = "Congratulations! You are in a good shape!"
+        } else if (java.lang.Float.compare(bmi, 25f) > 0 && java.lang.Float.compare(
+                bmi,
+                30f
+            ) <= 0
+        ) {
+            bmiLabel = "Overweight"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (java.lang.Float.compare(bmi, 30f) > 0 && java.lang.Float.compare(
+                bmi,
+                35f
+            ) <= 0
+        ) {
+            bmiLabel = "Obese Class | (Moderately obese)"
+            bmiDescription = "Oops! You really need to take care of your yourself! Workout maybe!"
+        } else if (java.lang.Float.compare(bmi, 35f) > 0 && java.lang.Float.compare(
+                bmi,
+                40f
+            ) <= 0
+        ) {
+            bmiLabel = "Obese Class || (Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        } else {
+            bmiLabel = "Obese Class ||| (Very Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        }
+
+        tvYourBMI.visibility = View.VISIBLE
+        tvBMIValue.visibility = View.VISIBLE
+        tvBMIType.visibility = View.VISIBLE
+        tvBMIDescription.visibility = View.VISIBLE
+
+        // This is used to round of the result value to 2 decimal values after "."
+        val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
+
+        tvBMIValue.text = bmiValue // Value is set to TextView
+        tvBMIType.text = bmiLabel // Label is set to TextView
+        tvBMIDescription.text = bmiDescription // Description is set to TextView
+
+    }
+
+    private fun validateMetricUnits(): Boolean {
+        var isValid = true
+
+        // If any of the two fields are empty return false
+        if (etMetricUnitWeight.text.toString().isEmpty())
+            isValid = false
+        else if (etMetricUnitHeight.text.toString().isEmpty())
+            isValid = false
+
+        return isValid
     }
 }
